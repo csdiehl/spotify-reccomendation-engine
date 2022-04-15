e92740f90d18f5767ea0db67aadf604c77c9d524
csdiehl <csdiehl16@gmail.com>
2022-04-15 09:02:17 -0700

    fixed spa issue in gh pages

--- src/components/GetAuth.js
+++ src/components/GetAuth.js
@@ -5,7 +5,7 @@ const GetAuth = () => {
   var url = "https://accounts.spotify.com/authorize";
   url += "?response_type=token";
   url += "&client_id=bf7e4aff5a884f51ac39072291709f0e"
-  url += `&redirect_uri=${window.location}redirect`
+  url += `&redirect_uri=${window.location.href}redirect`
 
   const getAuth = () => {
       window.location = url
