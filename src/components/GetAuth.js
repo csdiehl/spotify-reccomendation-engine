c4241ea63c00eee7cecfb2af422045c9436221c2
csdiehl <csdiehl16@gmail.com>
2022-04-14 23:42:59 -0700

    fixed window location auth error

--- src/components/GetAuth.js
+++ src/components/GetAuth.js
@@ -5,7 +5,7 @@ const GetAuth = () => {
   var url = "https://accounts.spotify.com/authorize";
   url += "?response_type=token";
   url += "&client_id=bf7e4aff5a884f51ac39072291709f0e"
-  url += "&redirect_uri=http://localhost:3000/redirect"
+  url += `&redirect_uri=${window.location}redirect`
 
   const getAuth = () => {
       window.location = url
